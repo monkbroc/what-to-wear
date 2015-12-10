0f0fc261ecaf417e36c6ba23731d5f9bd982e13b
Julien Vanier <jvanier@gmail.com>
2015-12-10 13:07:11 -0500

    Make manual mode simpler

--- firmware/weather_clock.cpp
+++ firmware/weather_clock.cpp
@@ -7,6 +7,8 @@
 
 SYSTEM_THREAD(ENABLED);
 
+void goToSleep();
+
 Pointer pointer(D0);
 WeatherPointer weatherPointer(pointer);
 Persistence persistence;
@@ -14,25 +16,75 @@ PointerPersistence pointerPersistence(pointer, persistence);
 PowerSupply powerSupply;
 
 const int CONNECT_DELAY_SECS = 30;
-const int SLEEP_DELAY_SECS = 60;
+const int SLEEP_DELAY_SECS = 2 * 60;
 const int SLEEP_TIME_SECS = 60 * 60;
 
-bool responseReceived = false;
-unsigned long sleepTimer = 0;
+bool manualMode = false;
+bool hookResponseReceived = false;
+unsigned long lastResponseTime = 0;
+
+bool wokeUpFromPin() {
+  pinMode(WKP, INPUT);
+  return digitalRead(WKP) == HIGH;
+}
+
+STARTUP(manualMode = wokeUpFromPin());
 
 int setClothes(String value) {
-  sleepTimer = millis();
+  Serial.println("Setting clothes " + value);
+  lastResponseTime = millis();
   weatherPointer.pointTo(value);
   return 0;
 }
 
-void setClothesHook(const char *event, const char *value) {
+void setClothesFromHook(const char *event, const char *value) {
   String clothes = String(value).trim();
-  // When clothes selection is manual, wait for the "clothes" cloud
-  // function to be called to move the pointer
-  if(clothes != "manual") {
-    responseReceived = true;
-    setClothes(clothes);
+  Serial.println("Received what-to-wear response");
+  hookResponseReceived = true;
+  setClothes(clothes);
+}
+
+void getClothesFromHook() {
+  Particle.subscribe("hook-response/what-to-wear", setClothesFromHook, MY_DEVICES);
+  if(waitFor(Particle.connected, CONNECT_DELAY_SECS * 1000)) {
+    Serial.println("Published what-to-wear event");
+    Particle.publish("what-to-wear", PRIVATE);
+  } else {
+    Serial.println("Couldn't connect => Go to sleep");
+    goToSleep();
+  }
+}
+
+void setup() {
+  Serial.begin(9600); delay(200);
+
+  powerSupply.setup();
+  Serial.println("Woke from " + String(manualMode ? "pin" : "timer/reset"));
+
+  pointerPersistence.setup();
+  Particle.function("clothes", setClothes);
+
+  if(!manualMode) {
+    getClothesFromHook();
+  }
+}
+
+unsigned long timeSinceLastResponse() {
+  return millis() - lastResponseTime;
+}
+
+bool readyToSleep() {
+  if(hookResponseReceived && !pointer.isMoving()) {
+    return true;
+  }
+
+  return timeSinceLastResponse() > SLEEP_DELAY_SECS * 1000;
+}
+
+void loop() {
+  if(readyToSleep()) {
+    Serial.println("Done waiting => Go to sleep"); delay(200);
+    goToSleep();
   }
 }
 
@@ -40,31 +92,3 @@ void goToSleep() {
   System.sleep(SLEEP_MODE_DEEP, SLEEP_TIME_SECS);
 }
 
-void setup() {
-  powerSupply.setup();
-
-  pointerPersistence.setup();
-  Particle.subscribe("hook-response/what-to-wear", setClothesHook, MY_DEVICES);
-  Particle.function("clothes", setClothes);
-
-  if(waitFor(Particle.connected, CONNECT_DELAY_SECS * 1000)) {
-    Particle.publish("what-to-wear", PRIVATE);
-  } else {
-    goToSleep();
-  }
-  sleepTimer = millis();
-}
-
-bool readyToSleep() {
-  if(responseReceived && !pointer.isMoving()) {
-    return true;
-  }
-
-  return ((millis() - sleepTimer) > SLEEP_DELAY_SECS * 1000);
-}
-
-void loop() {
-  if(readyToSleep()) {
-    goToSleep();
-  }
-}
