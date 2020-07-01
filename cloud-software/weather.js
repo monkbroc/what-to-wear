ccd0fb37aa444fd4bc2fe8a93d4abfcdb3c98dc0
Julien Vanier <jvanier@gmail.com>
2020-07-01 09:54:03 -0400

    Add weather functions

--- cloud-software/weather.js
+++ cloud-software/weather.js
@@ -1,9 +1,37 @@
 const got = require('got');
 
 function forecastUrl({ apiKey, lat, lon }) {
-	return `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&exclude=current,minutely,hourly`;
+	return `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&exclude=minutely,hourly&units=metric`;
+}
+
+async function getForecast({ url, _got = got }) {
+	const { body } = await _got(url);
+	return JSON.parse(body);
+}
+
+function clothesForWeather({ forecast }) {
+	// transforms weather data from https://openweathermap.org/api/one-call-api
+	// into clock predictions
+	const daily = forecast.daily[0];
+	const summary = daily.weather[0].main;
+	const temperature = forecast.current.feels_like;
+	if (summary === 'Thunderstorm' || summary === 'Drizzle' || summary === 'Rain') {
+		return 'umbrella';
+	} else if (summary === 'Snow') {
+		return 'shovel';
+	} else if (temperature < 2) {
+		return 'hat';
+	} else if (temperature < 12) {
+		return 'jacket';
+	} else if (temperature < 20) {
+		return 'sweater';
+	} else {
+		return 'tshirt';
+	}
 }
 
 module.exports = {
-	forecastUrl
+	forecastUrl,
+	getForecast,
+	clothesForWeather
 };
--- cloud-software/weather.test.js
+++ cloud-software/weather.test.js
@@ -6,7 +6,128 @@ describe('weather', () => {
 	describe('forecastUrl', () => {
 		it('returns a formatted url', () => {
 			const url = weather.forecastUrl({ apiKey: 'abc123', lat: 42, lon: -83 });
-			expect(url).to.eql('http');
+			expect(url).to.eql('https://api.openweathermap.org/data/2.5/onecall?lat=42&lon=-83&appid=abc123&exclude=minutely,hourly&units=metric');
+		});
+	});
+
+	describe('getForecast', () => {
+		it('returns payload', async () => {
+			const expectedUrl = 'https://weather';
+			const body = '{ "weather": "clear" }';
+			const got = async function(url) {
+				expect(url).to.eql(expectedUrl);
+				return { body };
+			};
+
+			const result = await weather.getForecast({ url: expectedUrl, _got: got });
+			expect(result).to.eql({ weather: 'clear' });
+		});
+	});
+
+	describe('clothesForWeather', () => {
+		it('maps thunderstorm to umbrella', () => {
+			const forecast = {
+				current: {
+					feels_like: 0
+				},
+				daily: [{
+					weather: [{
+						main: 'Thunderstorm'
+					}]
+				}]
+			};
+			expect(weather.clothesForWeather({ forecast })).to.eql('umbrella');
+		});
+		it('maps drizzle to umbrella', () => {
+			const forecast = {
+				current: {
+					feels_like: 0
+				},
+				daily: [{
+					weather: [{
+						main: 'Drizzle'
+					}]
+				}]
+			};
+			expect(weather.clothesForWeather({ forecast })).to.eql('umbrella');
+		});
+		it('maps rain to umbrella', () => {
+			const forecast = {
+				current: {
+					feels_like: 0
+				},
+				daily: [{
+					weather: [{
+						main: 'Rain'
+					}]
+				}]
+			};
+			expect(weather.clothesForWeather({ forecast })).to.eql('umbrella');
+		});
+		it('maps snow to shovel', () => {
+			const forecast = {
+				current: {
+					feels_like: 0
+				},
+				daily: [{
+					weather: [{
+						main: 'Snow'
+					}]
+				}]
+			};
+			expect(weather.clothesForWeather({ forecast })).to.eql('shovel');
+		});
+		it('maps freezing to hat', () => {
+			const forecast = {
+				current: {
+					feels_like: 0
+				},
+				daily: [{
+					weather: [{
+						main: 'Clear'
+					}]
+				}]
+			};
+			expect(weather.clothesForWeather({ forecast })).to.eql('hat');
+		});
+		it('maps cold to jacket', () => {
+			const forecast = {
+				current: {
+					feels_like: 6
+				},
+				daily: [{
+					weather: [{
+						main: 'Clear'
+					}]
+				}]
+			};
+			expect(weather.clothesForWeather({ forecast })).to.eql('jacket');
+		});
+		it('maps warm to sweater', () => {
+			const forecast = {
+				current: {
+					feels_like: 14
+				},
+				daily: [{
+					weather: [{
+						main: 'Clear'
+					}]
+				}]
+			};
+			expect(weather.clothesForWeather({ forecast })).to.eql('sweater');
+		});
+		it('maps hot to tshirt', () => {
+			const forecast = {
+				current: {
+					feels_like: 30
+				},
+				daily: [{
+					weather: [{
+						main: 'Clear'
+					}]
+				}]
+			};
+			expect(weather.clothesForWeather({ forecast })).to.eql('tshirt');
 		});
 	});
 });
